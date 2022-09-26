2dff49dca91c0e8c149db7d3dcbc5db9d867da7a
Yuvraj Singh Deora <76275812+YUVRAJ06singh08deora@users.noreply.github.com>
2022-09-26 15:21:40 +0530

    variables and buzzer code updated

--- Smart_theft_protection.ino
+++ Smart_theft_protection.ino
@@ -3,8 +3,8 @@ const int buzzer = 8;
 int echopin = 6;
 int trigpin = 7;
 
-int mesafe;
-int sure;
+int distance;
+int data;
 
 
 void setup()
@@ -23,36 +23,21 @@ void loop()
  digitalWrite(trigpin,HIGH);
  delayMicroseconds(10);
  digitalWrite(trigpin,LOW);
- sure = pulseIn(echopin,HIGH);
- mesafe = (sure/2)/29.0;
+ data = pulseIn(echopin,HIGH);
+ distance = (data/2)/29.0;
   
- if(mesafe <= 15)
+ if(distance >= 30)
  {
   digitalWrite(buzzer,HIGH);
-  delay(250);
-  digitalWrite(buzzer,LOW);
-  delay(125);
+ 
  }
- else if(mesafe <= 20)
- {
-  digitalWrite(buzzer,HIGH);
-  delay(500);
-  digitalWrite(buzzer,LOW);
-  delay(250);
- }
- else if(mesafe <= 30)
- {
-  digitalWrite(buzzer,HIGH);
-  delay(1000); 
-  digitalWrite(buzzer,LOW);
-  delay(1000);  
- }
+
  else
  {
   digitalWrite(buzzer,LOW);
  }
   Serial.print("Distance = ");
-  Serial.print(mesafe);
+  Serial.print(distance);
   Serial.println("cm");
   delay(500);
 }
